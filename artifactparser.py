73e57577412a9e0fcc63cbcfd5ec847981d1ad11
Andrew Iwamaye <andrew@Andrews-MacBook-Pro.local>
2024-05-09 18:06:09 -1000

    Fixed winevt logic.

--- artifactparser.py
+++ artifactparser.py
@@ -1,102 +1,135 @@
 from datetime import datetime
 import os
 import subprocess
+import sys
+import time
 
-desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
-arguments = " "
-zimmermanFolder = "./Zimmerman Tools/"
+# Variables
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+
+
+# 'https://stackoverflow.com/questions/4417546/constantly-print-subprocess-output-while-process-is-running'
+# function to execute a supplied command as a subprocess of this script
+def zimmermanExecute(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print(f"[+] Parsing with your supplied options: {command}.")
+    
+    while True:
+        nextline = process.stdout.readline()
+        result = process.poll()
+        if result is not None: 
+            process.terminate()
+            break
+        else:
+            sys.stdout.write(nextline.decode('utf8'))
+  
+    print(f"[+] DONE parsing with your supplied options: {command}.\n")
+    print("Please check the Desktop for the parsed CSV output file.\n")
+        
+    #Poll process for new output until finished
+    # while True:
+        # nextline = process.stdout.readline()
+        # if nextline == '' and process.poll() is not None:
+            # break
+        # sys.stdout.write(nextline.decode('utf8'))
+
+
+    # output = process.communicate()[0]
+    # exitCode = process.returncode
+
+    # if (exitCode == 0):
+        # return output
+    # else:
+        # raise ProcessException(command, exitCode, output)
+
 
 while True:
     # Initial Menu for user to pick from.
     while True:
         inputArtifactChoiceList = ["1", "2", "3", "4", "5", "6", "7"]
-        print("All parsed artifacts will be output to the Desktop as a CSV file.")
-        artifact = input("Enter a number from the menu below.\n\
-                        1. Windows Event Logs\n\
-                        2. MFT\n\
-                        3. MFT Timeline\n\
-                        4. SRUM\n\
-                        5. Prefetch\n\
-                        6. Amcache\n\
-                        7. IIS Logs\n\
-                        Choice: ")
-        
+        print("\nAll parsed artifacts will be output to the Desktop as a CSV file.\n")
+        artifact = input("""Enter a number from the menu below.\n1. Windows Event Logs\n2. MFT\n3. MFT Timeline\n4. SRUM\n5. Prefetch\n6. Amcache\n7. IIS Logs\n\nChoice: """)
+
         if artifact in inputArtifactChoiceList:
-               break
+            break
         else:
             print("Please enter a valid option.\n")
-    
 
 
     # WINDOWS EVENT LOGS LOGIC
     if artifact == "1":
         # Initilize an argument string to be fed into ZimmermanTool
+        arguments = " "
 
         # Define path of EvtxECmd.exe
-        zimmermanExe = zimmermanFolder + 'EvtxECmd.exe'
+        cwd = os.path.join(os.getcwd(), 'ZimmermanTools\\', 'EvtxECmd\\')
+        zimmermanExePath = os.path.join(cwd, 'EvtxECmd.exe')
         
         # Get hostname of asset being analyzed
         hostname = input("Enter hostname you are analyzing: ")
 
+        
         # Define output csv filename
         csvf = "evtx_" + hostname + ".csv"
-
-        # Determine if a directory or file needs to be parsed.
+       
+        # Determine if it is a directory or file that needs to be parsed.
         inputTypeChoiceList = ["d", "directory", "f", "file"]
         while True:
-            inputTypeChoice = input('Parsing a directory or a file?\n\
-                                    Enter "d" for directory or "f" for file: ')
-            
+            inputTypeChoice = input("""Parsing a directory or a file?\nEnter "d" for directory or "f" for file: """)
+
             if inputTypeChoice.lower() in inputTypeChoiceList:
-               break
-           
+                break
+
             else:
                 print("Please enter a valid option.\n")
-            
+
         # Intialize input arguments for ZimmermanTools
-        if inputTypeChoice == "d":
+        if inputTypeChoice.lower() == "d":
             dir = input("Enter directory path: ")
             arguments = arguments + "-d" + " " + '"' + dir + '"'
-        if inputTypeChoice == "f":
+        if inputTypeChoice.lower() == "f":
             f = input("Enter file path: ")
             arguments = arguments + "-f" + " " + '"' + f + '"'
 
         # Determine if a specific timerange is desired.
         inputTimeChoiceList = ["t", "a"]
         while True:
-            inputTime = input('Need a specific a timerange? Or do you want the entire input parsed?\n\
-               Enter "t" for a specific timerange or "a" for all timeranges: ')
-    
+            inputTime = input('Need a specific a timerange? Or do you want the entire input parsed?\nEnter "t" for a specific timerange or "a" for all timeranges: ')
+
             if inputTime.lower() in inputTimeChoiceList:
-               break
-            
+                break
+
             else:
                 print("Please enter a valid option.\n")
 
         # If timerange is selected, have user define start and end times.
         if inputTime == "t":
             while True:
-                inputStartTime = input('Start time (yyyy-MM-dd HH:mm:ss): ') 
-                inputEndTime = input('End time (yyyy-MM-dd HH:mm:ss): ')
-                
-                # Input validation
-                timeFormat = "%Y-%m-%d %H:%M:%S"
+                inputStartTime = input('Start time (yyyy-MM-dd): ')
+                inputEndTime = input('End time (yyyy-MM-dd): ')
 
-                if bool(datetime.strptime(inputStartTime, timeFormat)) and bool(datetime.strptime(inputEndTime, timeFormat)):
-                    # Intialize sd and ed input arguments for ZimmermanTools
-                    sd = inputStartTime + ".0000000"
-                    ed = inputEndTime + ".0000000"
-                    arguments = arguments + " " + "--sd" + " " + sd + " " + "--ed" + " " + ed
-                    break
+                # Input validation
+                timeFormat = "%Y-%m-%d"
+
+                try:
+                    if bool(datetime.strptime(inputStartTime, timeFormat)) and bool(datetime.strptime(inputEndTime, timeFormat)):
+                        # Intialize sd and ed input arguments for ZimmermanTools
+                        sd = inputStartTime
+                        ed = inputEndTime
+                        arguments = arguments + " " + "--sd" + " " + sd + " " + "--ed" + " " + ed
+                        break
+                except ValueError:
+                    print("Please enter a valid option.\n")
                 else:
                     print("Please enter a valid option.\n")
-
+                    
         # Construct final command
-        arguments = arguments + " " + "--csv" + + " " + '"' + desktop + '"' + " " + "--csvf" + " " + hostname
-        print("[+] Running EvtxECmd.exe with your supplied options.")
-        subprocess.run([zimmermanExe, arguments], capture_output = True, text = True)
-        print("\n * * * * * * * * * \n")
-        print("[+] DONE. Please check the Desktop for the parsed CSV output file.\n")
+        arguments = arguments + " " + "--csv" + " " + desktop + " " + "--csvf" + " " + csvf
+        command = zimmermanExePath + arguments
+
+        # Execute Zimmerman Tool
+        zimmermanExecute(command)
+
 
     # MFT LOGIC
 
@@ -109,13 +142,3 @@ while True:
     # AMCACHE LOGIC
 
     # IIS Logs
-
-
-
-
-
-
-
-
-   
-
